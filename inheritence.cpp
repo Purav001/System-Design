b71accfc86381555346b14b5cd6d66d2dbc289c6
Purav001 <puravsaluja4@gmail.com>
2025-09-05 12:24:37 +0530

    inheritence

---
+++ inheritence.cpp
@@ -0,0 +1,12 @@
+// for inheritence of a class
+// syntax:
+
+// class childClass : public parentClass
+
+// "childClass" can access all the methods,data of "parentClass"
+
+// instead of public we can use different access modifiers
+
+// public - everyone can access
+// private - only methods in class can access
+// protected - children methods can also access
